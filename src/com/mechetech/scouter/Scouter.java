ec3c2d7a9082fb51e256ec60ce32a06f0c140136
zjones3 <zjones3@ncsu.edu>
2020-05-10 10:51:48 -0400

    Initial build, plugin description file, and register grant command

--- src/com/mechetech/scouter/Scouter.java
+++ src/com/mechetech/scouter/Scouter.java
@@ -1,8 +1,14 @@
 package com.mechetech.scouter;
 
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.mechetech.scouter.commands.GrantScouter;
+
 /**
+ * 
  * @author Zachary Jones
  *
  */
@@ -11,11 +17,23 @@ public class Scouter extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		super.onEnable();
+		PluginDescriptionFile pdfFile = getDescription();
+		Logger logger = getLogger();
+		
+		getCommand("grantscouter").setExecutor(new GrantScouter());
+		
+		logger.info(pdfFile.getName() + " has been enabled (V." + 
+				pdfFile.getVersion() + ")");
 	}
 	
 	@Override
 	public void onDisable() {
 		super.onDisable();
+		PluginDescriptionFile pdfFile = getDescription();
+		Logger logger = getLogger();
+		
+		logger.info(pdfFile.getName() + " has been disabled (V." + 
+				pdfFile.getVersion() + ")");
 	}
 	
 }
