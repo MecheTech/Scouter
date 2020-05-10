1ba0ac839ce6d921b733a3a322d3d1a3e2179dfc
zjones3 <zjones3@ncsu.edu>
2020-05-10 08:08:56 -0400

    Connect to remote

---
+++ src/com/mechetech/scouter/Scouter.java
@@ -0,0 +1,21 @@
+package com.mechetech.scouter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Zachary Jones
+ *
+ */
+public class Scouter extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+	
+}
