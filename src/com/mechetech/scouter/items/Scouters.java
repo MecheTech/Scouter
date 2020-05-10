91d8f6b95608bcd5ad8906988686b03756b57551
zjones3 <zjones3@ncsu.edu>
2020-05-10 11:57:08 -0400

    Revert "refactor to use scouter as a sub class of ItemStack"
    This reverts commit d080fc70e0563df93866b9d6cdf85644a4798ae7.

--- src/com/mechetech/scouter/items/Scouters.java
+++ src/com/mechetech/scouter/items/Scouters.java
@@ -9,58 +9,25 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import net.md_5.bungee.api.ChatColor;
 
-public class Scouters extends ItemStack {
+public class Scouters {
 
-	/** The MC material scouter uses */
-	private static final Material SCOUTER_MATERIAL = Material.STICK;
+	private static Material scouterMaterial = Material.STICK;
 	
-	public Scouters () {
-		super(SCOUTER_MATERIAL);
+	public static ItemStack makeScouter() {
+		ItemStack scouter = new ItemStack(scouterMaterial);
+		ItemMeta meta = scouter.getItemMeta();
+		meta.setDisplayName(ChatColor.GRAY + "Scouter");
+		
+		List<String> lore = new ArrayList<>();
+		lore.add("This is a scouter!");
+		lore.add("Right click to discover mormoons within 250 blocks.");
+		lore.add("One use. No refunds!");
+		
+		return scouter;
 	}
-	
-	public class Scouter extends Scouters {
-		public Scouter() {
-			super();
-			ItemMeta meta = getItemMeta();
-			meta.setDisplayName(ChatColor.GRAY + "Scouter");
-			
-			List<String> lore = new ArrayList<>();
-			lore.add("This is a scouter!");
-			lore.add("Right click to discover mormoons within 250 blocks.");
-			lore.add("One use. No refunds!");
-		}
-	}
-	
-	public class MajorScouter extends Scouters {
-		public MajorScouter() {
-			super();
-			
-			ItemMeta meta = getItemMeta();
-			meta.setDisplayName(ChatColor.GRAY + "Major Scouter");
-			
-			List<String> lore = new ArrayList<>();
-			lore.add("This is a Major Scouter!");
-			lore.add("Right click to discover mormoons within 2000 blocks.");
-			lore.add("Many refunds!");
-			meta.setLore(lore);
-		}
-	}
-	
-//	public static ItemStack makeScouter() {
-//		ItemStack scouter = new ItemStack(scouterMaterial);
-//		ItemMeta meta = scouter.getItemMeta();
-//		meta.setDisplayName(ChatColor.GRAY + "Scouter");
-//		
-//		List<String> lore = new ArrayList<>();
-//		lore.add("This is a scouter!");
-//		lore.add("Right click to discover mormoons within 250 blocks.");
-//		lore.add("One use. No refunds!");
-//		
-//		return scouter;
-//	}
 	
 	public static ItemStack makeGreaterScouter() {
-		ItemStack scouter = new ItemStack(SCOUTER_MATERIAL);
+		ItemStack scouter = new ItemStack(scouterMaterial);
 		ItemMeta meta = scouter.getItemMeta();
 		meta.setDisplayName(ChatColor.GRAY + "Major Scouter");
 		
